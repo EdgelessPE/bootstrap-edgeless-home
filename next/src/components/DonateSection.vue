86bcf6420286326475b10e87742b67a2dc62e7e2
Cno <dsyourshy@qq.com>
2026-03-02 01:04:34 +0800

    fix: 样式

--- next/src/components/DonateSection.vue
+++ next/src/components/DonateSection.vue
@@ -1,26 +1,26 @@
 <template>
-  <section>
-    <div class="section">
-      <div class="container">
-        <div class="card-deck">
-          <div class="card pricing">
-            <div class="card-body text-center flex flex-col gap-4">
-              <h2 style="color: #2F5696">如果您认可<span class="font-earth">edgElEss</span>
-              </h2>
-              <h1 style="color: #2F5696">请帮助我们继续前行</h1>
-              <div>
-                <a href="https://wiki.edgeless.top/v2/global/donate.html" target="_blank">
-                  <img src="/img/pay.png" alt="" width="250" height="250" class="rounded" />
-                  <h4 style="color: #0072bc">支持支付宝、微信</h4>
-                </a>
-                <small style="color: #0072bc">点击二维码查看捐赠列表，如需要退款请联系我们</small>
-              </div>
-              <div class="mt-4">
-                <h3 style="color: #2F5696" class="mb-4">扫码付费过于直白？我们提供了额外的捐赠方式</h3>
-                <a href="/meal/index.html" target="_blank" class="btn btn-primary">让我康康</a>
-              </div>
-            </div>
+  <section class="w3l-index-block3 py-5">
+    <div class="section-info py-md-5">
+      <div class="container py-md-3">
+        <h1 class="h1 text-center h-auto" style="color: #2F5696">
+          如果您认可<span class="font-earth">edgElEss</span>
+          <br />
+          请帮助我们继续前行
+        </h1>
+
+        <div class="row justify-content-center">
+          <div class="col-md-6 text-center mb-5">
+            <a class="inline-block" href="https://wiki.edgeless.top/v2/global/donate.html" target="_blank">
+              <img src="/img/pay.png" alt="捐赠二维码" class="img-fluid rounded mb-3" style="max-width: 250px;" />
+              <h4 style="color: #0072bc">支持支付宝、微信</h4>
+            </a>
+            <small class="d-block" style="color: #0072bc">点击二维码查看捐赠列表，如需要退款请联系我们</small>
           </div>
+        </div>
+
+        <div class="text-center mt-4">
+          <h3 class="h4 mb-4" style="color: #2F5696">扫码付费过于直白？我们提供了额外的捐赠方式</h3>
+          <a href="/meal/index.html" target="_blank" class="btn btn-primary">让我康康</a>
         </div>
       </div>
     </div>
@@ -29,6 +29,12 @@
 </template>
 
 <style scoped>
+.section-info {
+  border: 1px solid #e0e0e0;
+  border-radius: 8px;
+  background-color: #fafafa;
+}
+
 .btn-primary {
   background-color: #2F5696;
   border-color: #2F5696;
