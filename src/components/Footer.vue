62ae413efd0483905ce85af7ee3c669260e0d713
Cno <dsyourshy@qq.com>
2026-03-02 19:25:49 +0800

    chore: update link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -5,7 +5,11 @@
         <a href="https://ks.wjx.top/vj/OSaSz3p.aspx" target="_blank" class="beta-btn">
           <i class="fas fa-external-link-alt"></i> 获取内测邀请码
         </a>
-        <a href="https://jq.qq.com/?_wv=1027&k=asq7UUHa" target="_blank" class="beta-btn beta-btn-secondary">
+        <a
+          href="https://jq.qq.com/?_wv=1027&k=asq7UUHa"
+          target="_blank"
+          class="beta-btn beta-btn-secondary"
+        >
           <i class="fas fa-users"></i> 加入内测交流2群（已满员）
           <span class="beta-qq">1146253726</span>
         </a>
@@ -16,28 +20,54 @@
       <div v-for="(links, category) in friendlyLinks" :key="category" class="mb-4">
         <span class="category-label d-block mb-2">{{ category }}</span>
         <div class="friend-links">
-          <a v-for="link in links" :key="link.url" :href="link.url" target="_blank" :title="link.title">{{ link.name
-            }}</a>
+          <a
+            v-for="link in links"
+            :key="link.url"
+            :href="link.url"
+            target="_blank"
+            :title="link.title"
+            >{{ link.name }}</a
+          >
         </div>
       </div>
     </div>
     <div class="footer-bottom">
       <div class="footer-links pt-4 pb-4">
         <div class="footer-link-item">
-          <svg t="1635599987840" class="icon" viewBox="0 0 1024 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-            p-id="2100" width="18" height="18">
+          <svg
+            t="1635599987840"
+            class="icon"
+            viewBox="0 0 1024 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="2100"
+            width="18"
+            height="18"
+          >
             <path
               d="M542.528 479.424c0-17.6 14.336-32.064 32-32.064s32 14.464 32 32.064S592.192 511.36 574.528 511.36 542.528 497.088 542.528 479.424zM857.792 915.264c-35.136 41.6-92.096 46.08-114.944 46.08-5.568 0-9.152-0.256-9.728-0.32l-182.016 0.064 0 0.064L288.192 961.152c-0.128 0-3.072 0.256-8.064 0.256-21.12 0-78.656-4.224-114.048-46.208-52.736-62.528-22.72-134.016-21.44-137.024l209.088-439.232L353.728 249.024C339.904 245.312 323.904 238.272 310.464 225.152 298.56 213.44 284.416 192.384 284.416 158.4c0-26.816 8.96-49.92 25.92-66.816 28.096-28.032 67.776-28.928 72.96-28.928 0 0 0.064 0 0.128 0l108.992 0 39.232 0 109.12 0c40.768 2.176 98.816 29.44 98.816 95.744 0 51.072-33.408 78.464-66.752 89.28l0.192 91.392 205.888 438.272C880.64 781.184 910.656 852.736 857.792 915.264zM278.464 646.016c32.384 14.272 99.904 32.64 200.768 1.152 110.144-34.432 200.064-34.304 255.808-28.48L609.024 346.24 608.704 190.016l31.168-0.832c10.56-0.448 35.712-4.48 35.712-30.784 0-27.008-28.16-31.168-36.864-31.808L531.648 126.656l0 0L383.232 126.656C370.368 126.848 348.416 131.328 348.416 158.4c0 14.272 5.184 19.392 6.912 21.056 8.448 8.32 24.256 10.048 27.392 9.728l32.576-0.576 2.496 32.576L414.656 359.936 278.464 646.016zM820.544 803.584l-53.376-115.392c-30.784-6.656-132.736-22.592-268.864 20.032-46.976 14.656-88.64 20.096-124.48 20.096-55.488 0-96.512-12.928-122.816-24.704l-48 100.928c0 0.192-14.72 37.504 12.096 69.312 18.56 21.952 57.344 23.872 70.656 23.296l187.072-0.064 0-0.064 262.912 0c16 1.28 54.656-1.28 73.216-23.168C835.968 841.856 820.736 803.968 820.544 803.584z"
-              fill="currentColor" p-id="2101"></path>
+              fill="currentColor"
+              p-id="2101"
+            ></path>
           </svg>
           <a href="#" @click.prevent="showBetaModal = true">获取内测</a>
         </div>
         <div class="footer-link-item">
-          <svg t="1635599866364" class="icon" viewBox="0 0 1088 1024" version="1.1" xmlns="http://www.w3.org/2000/svg"
-            p-id="2057" width="18" height="18">
+          <svg
+            t="1635599866364"
+            class="icon"
+            viewBox="0 0 1088 1024"
+            version="1.1"
+            xmlns="http://www.w3.org/2000/svg"
+            p-id="2057"
+            width="18"
+            height="18"
+          >
             <path
               d="M560 876.032a363.968 363.968 0 1 1 0-728.064 364.032 364.032 0 0 1 0 727.936v0.128zM112 512a448 448 0 1 0 896 0 448 448 0 0 0-896 0z m504-168a56 56 0 1 0-112 0 56 56 0 0 0 112 0z m-14.016 350.016a41.984 41.984 0 0 1-83.968 0V498.048a41.984 41.984 0 0 1 83.968 0v195.968z"
-              fill="currentColor" p-id="2058"></path>
+              fill="currentColor"
+              p-id="2058"
+            ></path>
           </svg>
           <a href="https://github.com/EdgelessPE/Edgeless/issues" target="_blank">问题反馈</a>
         </div>
@@ -66,7 +96,7 @@ const friendlyLinks = {
       title: "一个具有高拓展性的实验性质PE",
     },
     { name: "HotPE", url: "https://www.hotpe.top/", title: "" },
-    { name: "CowPE", url: "https://cowpe.top/", title: "" },
+    { name: "CowPE", url: "https://cowpe.cn/", title: "" },
     { name: "ComPE", url: "https://www.win-compe.top/", title: "" },
     { name: "Windsys Project", url: "https://windsys.whatk.me", title: "方便好用的非盈利Project" },
     { name: "ThirdWIN", url: "https://third.win/", title: "分享第三方社区 Windows 系统的部落" },
